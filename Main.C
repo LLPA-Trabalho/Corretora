4d00ada8ab6e394699c302edcb633001f0ed48ea
Lucas Alvim <lucas.alvim.lt@gmail.com>
2020-09-24 22:45:46 -0300

    Update Main.C

--- Main.C
+++ Main.C
@@ -1,40 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h> /* Adicionado a variavel boleana */
 #define MAX_CHARACTER 50
-
-typedef struct 
-{
-    int   ano_Construcao, ano_Reformado, id, modo;
-    bool  condominio, construido, vendido, reformado;
-    char  vendedor[MAX_CHARACTER], cliente[MAX_CHARACTER], tipo_De_Imovel[MAX_CHARACTER], bairro[MAX_CHARACTER], rua[MAX_CHARACTER];
-    float condominio_Valor, valor;
-    
-}Imovel;
-
-void Verificacao()
-{
-    
-}
-
 int main()
 {
-    int modo;
+    int   ano_Construcao, ano_Reformado, id, modo;
+    int   condominio, construido, vendido, reformado;
+    char  vendedor[MAX_CHARACTER], cliente[MAX_CHARACTER], tipo_De_Imovel[MAX_CHARACTER], bairro[MAX_CHARACTER], rua[MAX_CHARACTER];
+    float condominio_Valor, valor;
 
-    do{
-        system("cls");
-        printf("Digite o tipo de oporacao que voce deseja fazer\n");
-        printf("1 Para Comprar, 2 Para Adicionar um imovel e 3 para efetuar uma verificao\n");
-        scanf("%d",&modo);
-    }while(modo < 1 || modo > 3);
-
+    printf("Para cadastrar um imovel\nDigite 1\n");
+    scanf("%d",&modo);
     if(modo == 1){
 
-    }else if(modo == 2){
+        printf("Digite o nome do vendedor\n");
+        gets(vendedor);
 
+        puts("\nDigite o nome do comprador");
+
+        gets(cliente);
+
+        puts("\nDigite o tipo de imovel\n Apartamento ou Casa");
+
+        gets(tipo_De_Imovel);
+
+        puts("\nDigite o bairro do imovel");
+
+        gets(bairro);
+
+        puts("\nDigite a rua do imovel");
+
+        gets(rua);
+
+        puts("\nO Imovel ja foi construido ?\n 0 para sim e 1 para nao");
+
+        scanf("%d",&construido);
+
+        puts("\nA Imovel ja foi vendido ?\n 0 para sim e 1 para nao");
+
+        scanf("%d",&vendido);
+
+        puts("\nA Imovel esta em um condominio ?\n 0 para sim e 1 para nao");
+
+        scanf("%d",&condominio);
+
+        puts("\nA Imovel ja foi reformado ?\n 0 para sim e 1 para nao");
+
+        scanf("%d",&reformado);
+
+        puts("\nAno de construcao ou termino de construcao");
+
+        scanf("%d",&ano_Construcao);
+
+        if(ano_Reformado == 0){
+            puts("\nAno da reforma");
+
+            scanf("%d",&ano_Reformado);
+        }
+
+        puts("\nDigite o valor do imovel");
+
+        scanf("%f",&valor);
+
+        if(condominio == 0){
+            puts("\nDigite o valor do condominio");
+
+            scanf("%f",&condominio_Valor);
+        }
+
+        if(vendido == 1){
+            printf("\nO Vendedor :%s,",vendedor);
+            printf(" ira vender para o comprador:%s ",cliente);
+            printf("\nO(a) %s,",tipo_De_Imovel);
+            printf(" que esta localizado no bairro:%s",bairro);
+            printf("na rua:%s",rua);
+            if(construido == 0){
+                 printf("\nQue foi construida no ano de %d",ano_Construcao);
+            }else{
+                 printf("\nQue tera sua construcao encerrada no ano de %d",ano_Construcao);
+            }
+            if(reformado == 0){
+                printf("\nQue foi reformado no ano de %d",ano_Reformado);
+            }
+            if(condominio == 0){
+                printf("\nQue esta localizado em um condominio");
+                printf("\nO Valor total do imovel sera de %.2f + %.2f por mes por conta do condominio",valor,condominio_Valor);
+            }else{
+                printf("\nO Valor total do imovel sera de %.2f ",valor);
+            }
+
+        }else{
+            puts("Imovel vendido");
+        }
     }else{
-
+        printf("Entrada invalida");
     }
-
     return 0;
 }
